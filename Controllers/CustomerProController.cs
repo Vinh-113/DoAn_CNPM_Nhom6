17c6109307d85a3b24328950a6a4114a16441152
soildertacoo <soildertacoo@DESKTOP-SHI61HA>
2025-06-16 22:34:09 +0700

    Đã chỉnh phần CustomerPro, Index,DangKy

--- Controllers/CustomerProController.cs
+++ Controllers/CustomerProController.cs
@@ -22,12 +22,13 @@ namespace TechStore.Controllers
             {
                 var product = _context.Products.FirstOrDefault(s => s.ProductID == id);
                 var relatedProducts = _context.Products.Where(s => s.Category1.IDCate == product.Category1.IDCate).ToList();//Tìm sản phẩm tương tự nhưng có mã Catrgory giống như sản phẩm hồi nãy
-                var reviews = _context.Reviews.Where(s => s.ProductID == id).ToList();
+               /* var reviews = _context.Reviews.Where(s => s.ProductID == id).ToList();*/
+                
                 var relatedPro = new RelatedPro
                 {
-                    RelatedReviews = reviews,
+                    /*RelatedReviews = reviews,*/
                     Product = product,
-                    RelatedProducts = relatedProducts,
+                    RelatedProducts = relatedProducts
                 };
                 return View(relatedPro);
             }
